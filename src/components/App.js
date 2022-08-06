cfd8698da453d693f48a84bb589aee946b0ed36e
Mohamed Salman <mohamed.akj@gmail.com>
2022-08-06 17:21:17 +0530

    Added new tweet screen design

--- src/components/App.js
+++ src/components/App.js
@@ -4,23 +4,23 @@ import { LoadingBar } from "react-redux-loading-bar";
 
 import { handleInitialData } from "../actions/shared";
 import Dashboard from "./Dashboard";
-
+import NewTweet from "./NewTweet";
 
 const App = (props) => {
   useEffect(() => {
     props.dispatch(handleInitialData());
   }, []);
 
-  return <div>
-    <LoadingBar />
-    {props.loading ? null : <Dashboard />}
-  </div>;
+  return (
+    <div>
+      <LoadingBar />
+      {props.loading ? null : <NewTweet />}
+    </div>
+  );
 };
 
-const mapStateToProps = ({authedUser}) => (
-  {
-    loading: authedUser === null,
-  }
-);
+const mapStateToProps = ({ authedUser }) => ({
+  loading: authedUser === null,
+});
 
 export default connect(mapStateToProps)(App);
